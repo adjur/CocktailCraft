f3112c04a3ed94beec92c0a32452dec8fdc9f7fd
Andi Dj <155870242+adjur@users.noreply.github.com>
2024-06-05 10:16:22 -0700

    updates

--- components/CardSlider/CardSlider.jsx
+++ components/CardSlider/CardSlider.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import './CardSlider.css'
+import React from 'react';
+import './CardSlider.css';
+
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const CardSlider = () => {
   return (
     <section id='CardSlider' className='cardSlider'>
+      <div id='title-div'>
+        <div className='layer1'>
+          <h2 id='title'>Tried and True Crowd Pleasers</h2>
+        </div>
+      </div>
       <div className='main'>
-        <div className='body'>
-
+        <div className='scrolling-wrapper'>
+          <div className='card'><h2>Card</h2></div>
+          <div className='card'><h2>Card</h2></div>
+          <div className='card'><h2>Card</h2></div>
+          <div className='card'><h2>Card</h2></div>
+          <div className='card'><h2>Card</h2></div>
+          <div className='card'><h2>Card</h2></div>
+          <div className='card'><h2>Card</h2></div>
+          <div className='card'><h2>Card</h2></div>
         </div>
       </div>
     </section>
   )
 }
 
-export default CardSlider
+export default CardSlider;
