6db1db730b5c478cc4cd476c336cf6daab1960da
Andi Dj <155870242+adjur@users.noreply.github.com>
2024-06-06 18:44:09 -0700

    packages build

--- components/Packages/Packages.jsx
+++ components/Packages/Packages.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import './Packages.css'
 
 const Packages = () => {
   return (
-    <section className='main'>
-    <div>Packages</div>
+    <section id='Packages' className='packages'>
+      <div className='package-titles-div'>
+        <div className='package-layer1'>
+          <h1 className='package-titles'>Packages to Host</h1>
+        </div>
+      </div>
+      <div className='detailsParagraph'>
+        <p className='package-details'>
+          Comprehensive packages that include everything needed to craft delicious cocktails and entertain your guests, except the alcohol.</p>
+        <p className='package-details'>
+          Each package includes premium mixers, reusable recipe cards, and fresh fruit delivered right to your doorstep, ensuring your party is a hit without the hassle.
+        </p>
+      </div>
+      <div className='parentPackage'>
+        <div className='pack1'>Packages</div>
+        <div className='pack2'>Packages</div>
+        <div className='pack3'>Packages</div>
+      </div>
     </section>
   )
 }
